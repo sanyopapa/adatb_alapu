969fc93862169d66046d344d4aa1a0d26b2da192
Esztebán <bbx523@gmail.com>
2024-04-10 22:12:27 +0200

    Korrigáció a connection.php c. fájlban

--- Project/controller/connection.php
+++ Project/controller/connection.php
@@ -2,22 +2,22 @@
 
 <?php
 	$dbhost = "localhost";
-	$dbuser = "C##TESZTOLASZCS";
-	$dbpass = "C##TESZTOLASZCS";
+	$dbuser = "SYSTEM";
+	$dbpass = ""; //Íde kell a jelszó
 	$dbname = "csaladfa_db";
 	$tns = "
 (DESCRIPTION =
     (ADDRESS_LIST =
-      (ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.0.19)(PORT = 1521))
+      (ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.0.19)(PORT = 1521)) 
     )
     (CONNECT_DATA =
       (SID = xe)
     )
-  )";
+  )"; //Lehet az IP címet át kell írni. Ha gond lenne, jelezz! (Brúnó)
  
 
 
-	if(!$con =oci_connect('SYSTEM', 'Goldenapple1', $tns,'UTF8'))
+	if(!$con =oci_connect($dbuser, $dbpass, $tns,'UTF8'))
 	{
 		echo "Nem sikerült csatlakozni az adatbázishoz!";
 	}
