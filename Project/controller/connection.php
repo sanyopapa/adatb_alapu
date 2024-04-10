6aabc5f7c8c6346cc7d3b8bb24f17fbc2be6e0e6
Esztebán <bbx523@gmail.com>
2024-04-10 22:19:09 +0200

    Baszakodás 2

--- Project/controller/connection.php
+++ Project/controller/connection.php
@@ -3,7 +3,7 @@
 <?php
 	$dbhost = "localhost";
 	$dbuser = "SYSTEM";
-	$dbpass = "Goldenapple1"; //Íde kell a jelszó
+	$dbpass = ""; //Íde kell a jelszó
 	$dbname = "csaladfa_db";
 	$tns = "
 (DESCRIPTION =
@@ -13,7 +13,8 @@
     (CONNECT_DATA =
       (SID = xe)
     )
-  )"; 
+  )"; //Lehet az IP címet át kell írni. Ha gond lenne, jelezz! (Brúnó)
+ 
 
 
 	if(!$con =oci_connect($dbuser, $dbpass, $tns,'UTF8'))
