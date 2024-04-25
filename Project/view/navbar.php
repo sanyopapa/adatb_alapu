0611eb03863079b2e7a79d9b58fa0ad35959d019
Esztebán <bbx523@gmail.com>
2024-04-25 09:59:39 +0200

    Alacsonypadlós szerelvények kilistázásának megoldása SQL függvénnyel.

--- Project/view/navbar.php
+++ Project/view/navbar.php
@@ -36,7 +36,7 @@
         <a class="inactive_dropdown" href="passes.php">Bérletvásárlás</a>
         <a class="inactive_dropdown" href="osszes_jarat.php">Összes járat egy adott állomásról</a>
         <a class="inactive_dropdown" href="refresh_pwd_page.php">Adataid módosítása</a>
-       
+        <a class="inactive_dropdown" href="alacsonypadlos.php">Alacsonypadlós szerelvények</a>
       </div>
     </div>';
     if ($_SESSION["admin"] == 1) {
