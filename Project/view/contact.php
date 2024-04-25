ee8f56978ce8b066256c1aab4e909dc13f6e35fc
Marci <h269823@inf.u-szeged.hu>
2024-04-25 21:58:19 +0200

    a koronkénti leosztás bugos

--- Project/view/contact.php
+++ Project/view/contact.php
@@ -238,6 +238,169 @@
                         ?>
                     </td>
                 </tr>
+                <tr>
+                <td class="table_even">Kedvezményes vásárlásból származó bevétel</td>
+                <td class="table_even">
+                    <?php
+                            include("../controller/connection.php");
+
+                            $query = "SELECT SUM(Jegy.Ar) AS Osszeg
+                            FROM Felhasznalo
+                            JOIN Vasarol ON Felhasznalo.Email = Vasarol.Email
+                            JOIN Jegy ON Vasarol.Tipus = Jegy.Tipus
+                            WHERE Felhasznalo.Kedvezmenytipus = 'Diák' OR Felhasznalo.Kedvezmenytipus = 'Nyugdíjas'";
+
+                            $statement = oci_parse($con, $query);
+                            oci_execute($statement);
+
+                            if (oci_fetch($statement)) {
+                                $osszeg = oci_result($statement, "OSSZEG");
+                                if($osszeg == null){
+                                    echo "0 Ft";
+                                }else{
+                                    echo "" . $osszeg . " Ft";
+                                }
+                            }
+
+                            oci_close($con);
+                        
+                    ?>
+                </td>
+                </tr>
+                <tr>
+                    <td class="table_odd">Teljesárú vásárlásból származó bevétel</td>
+                    <td class="table_odd">
+                        <?php
+                                include("../controller/connection.php");
+
+                                $query = "SELECT SUM(Jegy.Ar) AS Osszeg
+                                FROM Felhasznalo
+                                JOIN Vasarol ON Felhasznalo.Email = Vasarol.Email
+                                JOIN Jegy ON Vasarol.Tipus = Jegy.Tipus
+                                WHERE Felhasznalo.Kedvezmenytipus = 'Felnőtt'";
+
+                                $statement = oci_parse($con, $query);
+                                oci_execute($statement);
+
+                                if (oci_fetch($statement)) {
+                                    $osszeg = oci_result($statement, "OSSZEG");
+                                    if($osszeg == null){
+                                        echo "0 Ft";
+                                    }else{
+                                        echo "" . $osszeg . " Ft";
+                                    }
+                                }
+
+                                oci_close($con);
+                            
+                        ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="table_even">Kedvezményes országbérletből származó bevétel</td>
+                    <td class="table_even ">
+                        <?php
+                                include("../controller/connection.php");
+
+                                $query = "SELECT SUM(Jegy.Ar) AS Osszeg
+                                FROM Felhasznalo
+                                JOIN Vasarol ON Felhasznalo.Email = Vasarol.Email
+                                JOIN Jegy ON Vasarol.Tipus = Jegy.Tipus
+                                WHERE Felhasznalo.Kedvezmenytipus = 'Diák' OR Felhasznalo.Kedvezmenytipus = 'Nyugdíjas' AND Jegy.Tipus IN ('Kedvezményes országbérlet')";
+
+                                $statement = oci_parse($con, $query);
+                                oci_execute($statement);
+
+                                if (oci_fetch($statement)) {
+                                    $osszeg = oci_result($statement, "OSSZEG");
+                                    if($osszeg == null){
+                                        echo "0 Ft";
+                                    }else{
+                                        echo "" . $osszeg . " Ft";
+                                    }
+                                }
+
+                                oci_close($con);
+                            
+                        ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="table_odd">Kedvezményes vármegyebérletből származó bevétel</td>
+                    <td class="table_odd">
+                        <?php
+                                include("../controller/connection.php");
+
+                                $query = "SELECT SUM(Jegy.Ar) AS Osszeg
+                                FROM Felhasznalo
+                                JOIN Vasarol ON Felhasznalo.Email = Vasarol.Email
+                                JOIN Jegy ON Vasarol.Tipus = Jegy.Tipus
+                                WHERE Felhasznalo.Kedvezmenytipus = 'Diák' OR Felhasznalo.Kedvezmenytipus = 'Nyugdíjas' AND Jegy.Tipus LIKE 'Kedvezményes vármegyebérlet%'";
+
+                                $statement = oci_parse($con, $query);
+                                oci_execute($statement);
+
+                                if (oci_fetch($statement)) {
+                                    $osszeg = oci_result($statement, "OSSZEG");
+                                    if($osszeg == null){
+                                        echo "0 Ft";
+                                    }else{
+                                        echo "" . $osszeg . " Ft";
+                                    }
+                                }
+
+                                oci_close($con);
+                            
+                        ?>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <br>
+        <table>
+            <thead>
+            <tr>
+                <th class = "table_header">Kor</th>
+                <th class = "table_header">Összesen költött</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            include("../controller/connection.php");
+
+            $query = "SELECT F.Eletkor, COALESCE(SUM(J.Ar), 0) AS Osszesen_koltott
+            FROM Felhasznalo F
+            LEFT JOIN Vasarol V ON F.Email = V.Email
+            LEFT JOIN Jegy J ON V.Tipus = J.Tipus
+            GROUP BY F.Eletkor";
+
+            $statement = oci_parse($con, $query);
+            oci_execute($statement);
+
+            $valtozo = 0;
+            while ($row = oci_fetch_assoc($statement)) {
+                $kor = $row['ELETKOR'];
+                $osszesenKoltott = isset($row['OSZESSEN_KOLTOTT']) ? $row['OSZESSEN_KOLTOTT'] : "0";
+                
+                if($valtozo % 2 == 0){
+                    echo "<tr>";
+                    echo "<td class='table_odd'>$kor</td>";
+                    echo "<td class='table_odd'>$osszesenKoltott Ft</td>";
+                    echo "</tr>";
+                }else{
+                    echo "<tr>";
+                    echo "<td class='table_even'>$kor</td>";
+                    echo "<td class='table_even'>$osszesenKoltott Ft</td>";
+                    echo "</tr>";
+                }
+                $valtozo++;
+            }
+            $valtozo = 0;
+
+            oci_close($con);
+            ?>
+
+
             </tbody>
         </table>
         </main>
